5fe1f9fe367086bba9f741e955a8041853263047
easyyglobal <easyyglobal@gmail.com>
2026-03-05 09:36:33 +0900

    ㅇ

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className="py-32">
       {/* Hero */}
-      <section className="bg-primary py-24 text-white">
+      <section className="bg-primary pt-[180px] pb-[100px] text-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
           <motion.h1
             initial={{ opacity: 0, y: 20 }}
